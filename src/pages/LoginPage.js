4f453f26a517eed500726a668dfa1f033a080de2
Gustavo Meira <gmeira.antonio@gmail.com>
2022-01-27 17:32:51 -0300

    feat: cria as verificacoes para o campo email e senha

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const LoginPage = () => (
-  <div>
-    <input
-      type="email"
-      data-testid="email-input"
-    />
-    <input
-      type="password"
-      data-testid="password-input"
-    />
-    <button type="button" data-testid="login-submit-btn">Enter</button>
-  </div>
-);
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const checkInputs = () => {
+    const MIN_LENGTH_PASSWORD = 6;
+    if (email.includes('@') && email.includes('.com')
+    && password.length > MIN_LENGTH_PASSWORD) return false;
+    return true;
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        data-testid="email-input"
+        onChange={ ({ target: { value } }) => setEmail(value) }
+        value={ email }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        onChange={ ({ target: { value } }) => setPassword(value) }
+        value={ password }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ checkInputs() }
+      >
+        Enter
+      </button>
+    </div>
+  );
+};
 
 export default LoginPage;
