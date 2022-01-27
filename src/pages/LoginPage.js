50c0a89c0907cf2397f46fda9af5b907b381f830
Gustavo Meira <gmeira.antonio@gmail.com>
2022-01-27 17:38:30 -0300

    feat: salva email da pessoa usuaria ao localStorage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -14,6 +14,7 @@ const LoginPage = () => {
   const handleButtonClick = () => {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
   };
 
   return (
