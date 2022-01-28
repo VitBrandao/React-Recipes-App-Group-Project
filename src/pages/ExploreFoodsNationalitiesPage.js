87e7396db60ae4c051e7f713643a51f09cabd146
Eduardomak <eduardomak92@gmail.com>
2022-01-28 01:48:57 -0300

    feat:cria footer

--- src/pages/ExploreFoodsNationalitiesPage.js
+++ src/pages/ExploreFoodsNationalitiesPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 const ExploreFoodsNationalitiesPage = () => (
   <div>
     <Header />
     <h1 data-testid="page-title">Explore Nationalities</h1>
+    <Footer />
   </div>
 );
 
