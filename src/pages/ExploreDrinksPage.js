cf613489ec8f60c002735ece4092bdc639250cdc
vitor <vitorsbsilva@gmail.com>
2022-01-31 18:32:02 -0300

    feat: reqs 70-73. falta 74

--- src/pages/ExploreDrinksPage.js
+++ src/pages/ExploreDrinksPage.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const ExploreDrinksPage = () => (
+const ExploreDrinksPage = ({ history }) => (
   <div>
     <Header showSearchButton={ false } />
     <h1 data-testid="page-title">Explore Drinks</h1>
+
+    <button
+      type="button"
+      data-testid="explore-by-ingredient"
+      onClick={ () => history.push('/explore/drinks/ingredients') }
+    >
+      By Ingredient
+    </button>
+
+    <button
+      type="button"
+      data-testid="explore-surprise"
+    >
+      Surprise me!
+    </button>
+
     <Footer />
   </div>
 );
+
+ExploreDrinksPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default ExploreDrinksPage;
--- src/pages/ExploreFoodsPage.js
+++ src/pages/ExploreFoodsPage.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const ExploreFoodsPage = () => (
+const ExploreFoodsPage = ({ history }) => (
   <div>
     <Header showSearchButton={ false } />
     <h1 data-testid="page-title">Explore Foods</h1>
+
+    <button
+      type="button"
+      data-testid="explore-by-ingredient"
+      onClick={ () => history.push('/explore/foods/ingredients') }
+    >
+      By Ingredient
+    </button>
+
+    <button
+      type="button"
+      data-testid="explore-by-nationality"
+      onClick={ () => history.push('/explore/foods/nationalities') }
+    >
+      By Nationality
+    </button>
+
+    <button
+      type="button"
+      data-testid="explore-surprise"
+    >
+      Surprise me!
+    </button>
+
     <Footer />
   </div>
 );
+
+ExploreFoodsPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default ExploreFoodsPage;
