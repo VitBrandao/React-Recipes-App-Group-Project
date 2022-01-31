7c803b361998a656589ce171d847361fb2e852e5
Gustavo Meira <gmeira.antonio@gmail.com>
2022-01-31 16:11:25 -0300

    fix: tentando arrumar requisitos

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ function defaultApi(URL) {
   return fetch(URL)
     .then((result) => result.json())
     .then((json) => json)
-    .catch((e) => console.log(e));
+    .catch((e) => e);
 }
 
 export default defaultApi;
