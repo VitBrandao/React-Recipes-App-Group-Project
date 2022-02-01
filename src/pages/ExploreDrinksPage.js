0885bac4ea3d6863c4f8db6665202154cfd20ee0
vitor <vitorsbsilva@gmail.com>
2022-02-01 09:33:45 -0300

    feat: explore meals or drinks page

--- src/pages/ExploreDrinksPage.js
+++ src/pages/ExploreDrinksPage.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultApi from '../services/index';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const ExploreDrinksPage = ({ history }) => (
-  <div>
-    <Header showSearchButton={ false } />
-    <h1 data-testid="page-title">Explore Drinks</h1>
+function ExploreDrinksPage({ history }) {
+  const findRandomPage = async () => {
+    const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    const findRandomDrink = await defaultApi(URL);
+    const randomDrinkId = findRandomDrink.drinks[0].idDrink;
 
-    <button
-      type="button"
-      data-testid="explore-by-ingredient"
-      onClick={ () => history.push('/explore/drinks/ingredients') }
-    >
-      By Ingredient
-    </button>
+    history.push(`/drinks/${randomDrinkId}`);
+  };
 
-    <button
-      type="button"
-      data-testid="explore-surprise"
-    >
-      Surprise me!
-    </button>
+  return (
+    <div>
+      <Header showSearchButton={ false } />
+      <h1 data-testid="page-title">Explore Drinks</h1>
 
-    <Footer />
-  </div>
-);
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+      >
+        By Ingredient
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ findRandomPage }
+      >
+        Surprise me!
+      </button>
+
+      <Footer />
+    </div>
+  );
+}
 
 ExploreDrinksPage.propTypes = {
   history: PropTypes.shape({
--- src/pages/ExploreFoodsPage.js
+++ src/pages/ExploreFoodsPage.js
@@ -1,39 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultApi from '../services/index';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const ExploreFoodsPage = ({ history }) => (
-  <div>
-    <Header showSearchButton={ false } />
-    <h1 data-testid="page-title">Explore Foods</h1>
+function ExploreFoodsPage({ history }) {
+  const findRandomPage = async () => {
+    const URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const findRandomFood = await defaultApi(URL);
+    const randomFoodId = findRandomFood.meals[0].idMeal;
 
-    <button
-      type="button"
-      data-testid="explore-by-ingredient"
-      onClick={ () => history.push('/explore/foods/ingredients') }
-    >
-      By Ingredient
-    </button>
+    history.push(`/foods/${randomFoodId}`);
+  };
 
-    <button
-      type="button"
-      data-testid="explore-by-nationality"
-      onClick={ () => history.push('/explore/foods/nationalities') }
-    >
-      By Nationality
-    </button>
+  return (
+    <div>
+      <Header showSearchButton={ false } />
+      <h1 data-testid="page-title">Explore Foods</h1>
 
-    <button
-      type="button"
-      data-testid="explore-surprise"
-    >
-      Surprise me!
-    </button>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
 
-    <Footer />
-  </div>
-);
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ findRandomPage }
+      >
+        Surprise me!
+      </button>
+
+      <Footer />
+    </div>
+  );
+}
 
 ExploreFoodsPage.propTypes = {
   history: PropTypes.shape({
